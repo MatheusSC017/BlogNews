86045c75cdfadaf281d0379fb812e03a335e11a6
unknown <matheussimao2101@gmail.com>
2022-07-05 08:32:48 -0300

    Página index últimas pesquisas realizadas

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
+from django.db.models import Max, Count, Subquery, OuterRef
 from django.utils import timezone
 from post import models as model_post
 from album import models as model_album
@@ -15,13 +16,23 @@ class Home(TemplateView):
         context['posts'] = model_post.Post.objects.filter(
             published_post=True,
             published_date_post__lte=timezone.now()
-        ).order_by('-published_date_post')[:2]
+        ).defer('description_post').order_by('-published_date_post')[:2]
+
         context['galery'] = model_album.Image.objects.filter(
             album_image__published_album=True
         ).order_by('?')[:6]
+
+        options = model_search.Option.objects.filter(
+            search_option=OuterRef('pk')
+        ).annotate(
+            vote_option=Count('vottinguseroption')
+        ).order_by('-vote_option')[:1]
         context['searches'] = model_search.Search.objects.filter(
             published_search=True,
-        )
+            publication_date_search__lte=timezone.now()
+        ).annotate(
+            max_option=Subquery(options.values('response_option'))
+        )[:3]
 
         return context
 
