e8a819b2be10c0181af229206964c86cbe903fe2
unknown <matheussimao2101@gmail.com>
2022-07-04 22:03:19 -0300

    Página index últimos 2 posts e imagens aleatórias da galeria

--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,29 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
+from django.utils import timezone
+from post import models as model_post
+from album import models as model_album
+from search import models as model_search
 
 
 class Home(TemplateView):
     template_name = 'blog/home.html'
+
+    def get_context_data(self, *args, **kwargs):
+        context = super().get_context_data(*args, **kwargs)
+
+        context['posts'] = model_post.Post.objects.filter(
+            published_post=True,
+            published_date_post__lte=timezone.now()
+        ).order_by('-published_date_post')[:2]
+        context['galery'] = model_album.Image.objects.filter(
+            album_image__published_album=True
+        ).order_by('?')[:6]
+        context['searches'] = model_search.Search.objects.filter(
+            published_search=True,
+        )
+
+        return context
 
 
 def about(request):
