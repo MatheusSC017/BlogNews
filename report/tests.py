65222f2e0f77b52c1a2fd2cf24978035470f1b3c
unknown <matheussimao2101@gmail.com>
2022-07-25 07:59:52 -0300

    Testes para a área administrativa do App Report

--- report/tests.py
+++ report/tests.py
@@ -1,3 +1,59 @@
 from django.test import TestCase
+from django.shortcuts import reverse
+from django.http import HttpRequest
+from django.contrib.auth.models import User
+from django.utils import lorem_ipsum
+from .models import Report
+from user.models import UserReportRegister
 
-# Create your tests here.
+
+class ReportActionsTestCase(TestCase):
+    def setUp(self):
+        self.client = self.client_class()
+
+        self.admin = User.objects.create_superuser(username='admin', email='admin@admin.com', password='admin123456')
+        self.user = User.objects.create_user(username='username_test', email='email@test.com', password='password_test')
+        self.report = Report.objects.create(user_report=self.user, description_report=lorem_ipsum.paragraph())
+
+    def test_approve_report(self):
+        self.client.login(username='admin', password='admin123456', request=HttpRequest())
+        response = self.client.post(reverse('admin:report_report_changelist'),
+                                    {
+                                        'action': 'approve_report',
+                                        '_selected_action': [self.report.pk, ]
+                                    })
+        self.assertEqual(response.status_code, 302)
+        self.assertRedirects(response, reverse('admin:report_report_changelist'))
+        report = Report.objects.get(pk=self.report.pk)
+        self.assertEqual(report.status_report, 'a')
+        user = User.objects.get_by_natural_key(self.user.username)
+        self.assertEqual(user.userreportregister.reports_userreportregister, 1)
+
+    def test_approve_report_user_with_3_reports(self):
+        UserReportRegister.objects.create(user_userreportregister=self.user,
+                                          reports_userreportregister=2)
+        self.client.login(username='admin', password='admin123456', request=HttpRequest())
+        response = self.client.post(reverse('admin:report_report_changelist'),
+                                    {
+                                        'action': 'approve_report',
+                                        '_selected_action': [self.report.pk, ]
+                                    })
+        self.assertEqual(response.status_code, 302)
+        self.assertRedirects(response, reverse('admin:report_report_changelist'))
+        report = Report.objects.get(pk=self.report.pk)
+        self.assertEqual(report.status_report, 'a')
+        user = User.objects.get_by_natural_key(self.user.username)
+        self.assertEqual(user.userreportregister.reports_userreportregister, 3)
+        self.assertEqual(user.userreportregister.status_userreportregister, 'b')
+
+    def test_reject_report(self):
+        self.client.login(username='admin', password='admin123456', request=HttpRequest())
+        response = self.client.post(reverse('admin:report_report_changelist'),
+                                    {
+                                        'action': 'reject_report',
+                                        '_selected_action': [self.report.pk, ]
+                                    })
+        self.assertEqual(response.status_code, 302)
+        self.assertRedirects(response, reverse('admin:report_report_changelist'))
+        report = Report.objects.get(pk=self.report.pk)
+        self.assertEqual(report.status_report, 'r')
